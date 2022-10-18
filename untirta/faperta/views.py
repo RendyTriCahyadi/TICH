2532805fbef559112a177345f0159194a9d2ba69
RendyTriCahyadi <you@example.com>
2022-10-18 13:33:04 +0700

    firsy commit

--- untirta/faperta/views.py
+++ untirta/faperta/views.py
@@ -2,4 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 def faperta(request):
+ dosen = Dosen.objects.all()
+    tenagaPendidik = Tenaga_Pendidik.objects.all()
+    mahasiswa = Mahasiswa.objects.all()
+
+    context = {
+        'dataDosen': dosen,
+        'dataTenagaPendidik': tenagaPendidik,
+        'dataMahasiswa': mahasiswa,
+    }
+
     return render(request, 'indexfaperta.html')
